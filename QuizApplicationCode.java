57f3253e9282b60d0f8651e58827dc12cbb6d22c
jainab mekala <jainabmekala31@gmail.com>
2026-04-16 13:09:12 +0530

    Update and rename GithubQuizApplication.java to QuizApplicationCode.java

--- QuizApplicationCode.java
+++ QuizApplicationCode.java
@@ -1,6 +1,6 @@
 package javaplacement;
 import java.util.Scanner;
-public class GithubQuizApplication {
+public class QuizApplicationCode {
 
 	public static void main(String[] args) {
 		        Scanner sc = new Scanner(System.in);
